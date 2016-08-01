fd60056c4176da7ad11a8bc6e17b32f644d33bcf
Julia Reshetnikova <lee.onore@gmail.com>
2016-08-01 16:44:16 +0300

    111

--- src/main/java/test3/App.java
+++ src/main/java/test3/App.java
@@ -1,15 +1,14 @@
 package test3;
 
-import org.jetbrains.Capitolio.*
+import org.jetbrains.Capitolio.*;
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
         Host host = new LocalHost();
     }
 }
