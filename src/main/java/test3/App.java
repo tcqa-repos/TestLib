79016388de30fc44e6d47a2dbf1444bb9b4954af
Julia Reshetnikova <lee.onore@gmail.com>
2016-08-01 16:42:24 +0300

    123

---
+++ src/main/java/test3/App.java
@@ -0,0 +1,15 @@
+package test3;
+
+import org.jetbrains.Capitolio.*
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Host host = new LocalHost();
+    }
+}
